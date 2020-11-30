6957780706ef453ccb365ce14d157c670068adfa
phamthomas12 <phamthomas12@gmail.com>
2020-11-30 21:21:40 +1100

    Final Draft

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,12 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
-var specialChar = "!@#$%^&*()<>?{}|_+,./;'[]`-=";
+var specialChar = "!@#$%^*()?{}|_+,./;'[]`-";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
@@ -19,55 +18,78 @@ function generatePassword() {
   //Password Length Alert
   var userLength = prompt("How many charecters would you like you password to be? \nMust be between 8 - 128 charecters ");
   if (userLength < 8 || userLength > 128) {
-    var lengthIncorrect = alert("Invalid password length request Please choose a length between 8 - 128")
+    var lengthIncorrect = alert("Invalid password length request Please choose a length between 8 - 128. Please try again");
   }
 
   //Password Length Decider
-  if (userLength >= 8 === userLength <= 128) {
+  if (userLength >= 8 && userLength <= 128) {
 
     // Lower Case Request Prompt
     var userLow = confirm ("Would you like the password to contain lower case letters?");
     if (userLow) {
-      alert (" You have selected you want lower case letters")
+      alert (" You have selected you want lower case letters");
     }
     else {
-      alert ("You have chosen not to use lower case letters")
+      alert ("You have chosen not to use lower case letters");
     }
 
     // Upper Case Request Prompt
     var userUpper = confirm ("Would you like the password to contain upper case letters?");
     if (userUpper) {
-      alert (" You have selected you want upper case letters")
+      alert (" You have selected you want upper case letters");
     }
     else {
-      alert ("You have chosen not to use upper case letters")
+      alert ("You have chosen not to use upper case letters");
     }
 
     // Numbers Request Prompt
     var userNumber = confirm ("Would you like the password to contain numbers?");
     if (userNumber) {
-      alert (" You have selected you want numbers")
+      alert (" You have selected you want numbers");
     }
     else {
-      alert ("You have chosen not to use numbers")
+      alert ("You have chosen not to use numbers");
     }
 
     // Special Charecter Request Prompt
     var userSpecial = confirm ("Would you like the password to contain special charecters?");
     if (userSpecial) {
-      alert (" You have selected you want special charecters")
+      alert (" You have selected you want special charecters");
     }
     else {
-      alert ("You have chosen not to use special charecters")
+      alert ("You have chosen not to use special charecters");
     }
-
+    // Password Requirement Enforcer
     if ( userLow === false && userUpper === false && userNumber === false && userSpecial === false) {
-      alert ("You need to select at least one character type before a password can be generated")
+      alert ("You need to select at least one character type before a password can be generated \nPlease press Generate Password button again to try again");
     }
   }
-    
   
+  //Password Criteria Combiner
+  var passwordCombiner = "";
+
+  if (userLow) {
+    passwordCombiner += lowercase;
+  }
+  if(userUpper) {
+    passwordCombiner += uppercase;
+  }
+  if (userNumber) {
+    passwordCombiner += numbers;
+  }
+  if (userSpecial) {
+    passwordCombiner += specialChar;
+  }
+
+  //Password Generator function
+  password = "";
+
+  for (i = 1; i <= userLength; i++) {
+    var passGen = Math.round(Math.random() * passwordCombiner.length);
+    password += passwordCombiner.charAt(passGen);
+  }
+  return password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# phamthomas12.github.io
+# 03 JavaScript: Password Generator
+
+Purpose: This webpage will generate a password based on specific criterias through the use of alerts.
+
+How it works: When the user presses the "Generate Password" button, a series of alerts will appear and ask the user on what they want the password to contain and also the legnth of the password.
+
+Functionality: Each time the user presses "OK", a reconfirmation statement will appear based on whether they press "OK" for yes or "Cancel" for no
+
+The user will be asked the following questions in order like:
+
+1. How many charecters between 8 and 128, if they do not meet this criteria then a error aleart will appear
+2. If they would like lower case letters
+3. If they would like upper case letters
+4. If they would like numbers
+5. If they would like special charecters
+
+After the five questions, a generated password will replace where "Your Secure Password" used to be
+
+If the user did not choose at least one character type, an alert will appear telling the user so, after that the user would have to press "Generate Password" again.
+
+![Start Page](Assets/StartPage.png)
+![Sample Alert](Assets/SampleAlert.png)
+![Confirmation](Assets/Confirmation.png)
+![Fail](Assets/Fail.png)
+![Result](Assets/Result.png)
+
+
+
+
